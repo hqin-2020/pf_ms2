40a4fe407c14822fc64c9d26aee80e57fa39171f
hqin-2020 <uctphqi@ucl.ac.uk>
2022-07-14 09:29:23 -0500

    2

---
+++ src/copy_final.py
@@ -0,0 +1,23 @@
+import shutil
+import os
+from tqdm import tqdm
+
+workdir = os.path.dirname(os.getcwd())
+source_dir = '/project2/lhansen/pf_ms2/'
+destination_dir = workdir + '/output/'
+
+N = 100_000
+T = 283
+batch_num = 150
+
+for i in tqdm(range(1,batch_num)):
+    case = 'actual data, seed = ' + str(i) + ', T = ' + str(T) + ', N = ' + str(N)
+    casedir = destination_dir + case  + '/'
+    try:
+        os.mkdir(casedir)
+        shutil.copy(source_dir + case  + '/θ_50.pkl', casedir)
+        shutil.copy(source_dir + case  + '/θ_100.pkl', casedir)
+        shutil.copy(source_dir + case  + '/θ_200.pkl', casedir)
+        shutil.copy(source_dir + case  + '/θ_282.pkl', casedir)
+    except:
+        pass
