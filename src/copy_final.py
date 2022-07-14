82bca7a8d8dd40c98af046e49ee93e30011fbf40
hqin-2020 <uctphqi@ucl.ac.uk>
2022-07-14 09:53:01 -0500

    1

--- src/copy_final.py
+++ src/copy_final.py
@@ -8,16 +8,12 @@ destination_dir = workdir + '/output/'
 
 N = 100_000
 T = 283
-batch_num = 150
 
-for i in tqdm(range(150,151)):
+for i in tqdm(range(1,151)):
     case = 'actual data, seed = ' + str(i) + ', T = ' + str(T) + ', N = ' + str(N)
     casedir = destination_dir + case  + '/'
     try:
         os.mkdir(casedir)
-        shutil.copy(source_dir + case  + '/θ_50.pkl', casedir)
-        shutil.copy(source_dir + case  + '/θ_100.pkl', casedir)
-        shutil.copy(source_dir + case  + '/θ_200.pkl', casedir)
         shutil.copy(source_dir + case  + '/θ_282.pkl', casedir)
     except:
         pass
